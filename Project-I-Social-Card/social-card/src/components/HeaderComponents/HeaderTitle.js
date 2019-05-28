5305352c74b9af55bc71040e3bea74d24f2a7411
LadyKerr <kadiekerr@gmail.com>
2019-05-28 13:34:57 -0400

    install momentjs & updated time

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Header.css';
 import HeaderContent from './HeaderContent';
-
+import moment from 'moment';
 
 const HeaderTitle = () => {
   return (
     <div className="header-title-div">
       <div className="header-title">
         <p>
-          <strong>Lambda School</strong> @LambdaSchool <span className="time">*8 Jan</span>
+          <strong>Lambda School</strong> @LambdaSchool <span className="time">{moment().format("MMM Do YYYY, h:mm a")}</span>
         </p>
         <HeaderContent />
       </div>

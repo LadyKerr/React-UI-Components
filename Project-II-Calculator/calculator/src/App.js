5cbf5f73d4e8bcd18c959ce46dc25dbaf24b1fa9
LadyKerr <kadiekerr@gmail.com>
2019-05-28 16:54:39 -0400

    calc text coming together

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import './App.css';
 
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
+
+
 const App = () => {
   return (
-    <div>
-      
+    <div className="calc-container">
+      <div className="calc-top-black">
+        <CalculatorDisplay text="0" />
+      </div>
+
+      <div className="calc-main">
+        <ActionButton text="clear" />
+        <div className="calc-nine">
+          <div className="row-one">
+            <NumberButton text="7" />
+            <NumberButton text="8" />
+            <NumberButton text="9" />
+          </div>
+          <div className="row-two">
+            <NumberButton text="4" />
+            <NumberButton text="5" />
+            <NumberButton text="6" />
+          </div>
+          <div className="row-three">
+            <NumberButton text="1" />
+            <NumberButton text="2" />
+            <NumberButton text="3" />
+          </div>
+        </div>
+      </div>
+
+        <ActionButton />
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Display.css';
 
-const CalculatorDisplay = () => {
+const CalculatorDisplay = props => {
   return (
-    <div className="calc-display">0</div>
+    <div className="calc-display">{props.text}</div>
   );
 };
 
